3aa47161cc8358ccf68cd081f7b8b04668a299b9
gga01075 <54846612+gga01075@users.noreply.github.com>
2021-01-08 09:10:25 +0900

    2021.01.08  포트폴리오 섹션

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,7 +9,6 @@ $(document).ready(function(){
             $('#conceptImg').hide();
         }
         num++;
-        console.log(num);
         $('#conceptImg img').eq(num).show().siblings().hide();
     },300);
 
@@ -60,13 +59,41 @@ _home.find('#rightText').on({
 
 /* about버튼 클릭이벤트 */
 _home.find('#leftText').on('click',function(){
-    $('#mainWrap').stop().animate({left:'350%'},8000,'easeInExpo');
-    $('#aboutWrap').stop().animate({left:0},8000,'easeInExpo');
-    $('#about img').addClass('bright');
-    setTimeout(function(){
-        $('#move_about').addClass('opa0');
-    },7500);
+    $('#mainWrap').stop().animate({left:'350%'},9000,'easeInCubic');
+    $('#aboutWrap').stop().animate({left:0},9000,'easeInCubic');
 });
+
+
+/* 포트폴리오 섹션 */
+/* pc web 스크린 position변경 */
+var scrollTimer = 0;
+var pf1_pos = $('#pf1').offset().top;
+var screenH = $('.pc_web .screen').outerHeight();
+var screenTop = pf1_pos - screenH;
+$(window).on('scroll',function(){
+    clearTimeout(scrollTimer);
+    scrollTimer = setTimeout(function(){
+        // console.log($(this).scrollTop());
+        if($(this).scrollTop() < pf1_pos){
+            $('.pc_web .screen').css({position:'absolute',top: screenTop});
+        }else if($(this).scrollTop() < pf1_pos*2){
+            $('.pc_web .screen').css({position:'fixed',top: screenTop});
+        }else{
+            $('.pc_web .screen').css({position:'absolute',top:pf1_pos + screenTop});
+        }
+
+        if($(this).scrollTop() < 1493){
+            $('.pc_web .screen img').attr({'src':'assets/images/portfolio/jaws_screen.jpg','alt':'죠스떡볶이 메인화면'});
+        }else{
+            $('.pc_web .screen img').attr({'src':'assets/images/portfolio/pubg_screen.png','alt':'PUBG 메인화면'});
+        }
+    },10);
+});
+
+/* pf3 title 위치조정 */
+var pointHei = $('#pf3 .pf_point').css('height');
+console.log(pointHei);
+$('#pf3 .pf_tit').css({bottom:378});
 
 
 
